50948f2385d98cba48a398b94dd4f59f13bcd0c8
vijaySriram <kumars.vijay05@gmail.com>
2024-08-08 23:15:12 +0530

    Commit for project

---
+++ src/test/java/testRun/SampleTest.java
@@ -0,0 +1,10 @@
+package testRun;
+
+import org.testng.annotations.Test;
+public class SampleTest {
+
+    @Test()
+    public void test() {
+       System.out.println("The welcome screen");
+    }
+}
